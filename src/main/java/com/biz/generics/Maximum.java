8bad0bec6d5ff5c8d1822564885d353b0c65de52
mkaubr007 <mkaubr007@gmail.com>
2021-09-07 22:04:56 +0530

    CheckMaxByMethod added

--- src/main/java/com/biz/generics/Maximum.java
+++ src/main/java/com/biz/generics/Maximum.java
@@ -1,18 +1,20 @@
 package com.biz.generics;
 
 public class Maximum{
-    public static void main(String[] args) {
-        String a="ab",b="cd",c="ef";
-        System.out.println(getMaximum(a,b,c));
-    }
-    public static String getMaximum(String a,String b,String c){
-        String max=a;
+
+
+    public static  <E extends Comparable> E getMaximum(E a, E b, E c){
+        E max=a;
         if(b.compareTo(a)>0 && b.compareTo(a)>0){
             max=b;
         }else if(c.compareTo(max)>0){
             max=c;
         }
+        printMax(a,b,c,max);
         return max;
+    }
+    public static <E> void printMax(E a,E b,E c,E max){
+        System.out.println("Max "+a+" "+b+" "+c+" "+max);
     }
 }
 
--- src/test/java/com/biz/generics/MaximumTest.java
+++ src/test/java/com/biz/generics/MaximumTest.java
@@ -5,10 +5,19 @@ import org.junit.Test;
 
 public class MaximumTest {
     @Test
+    public void checkMaximumInteger(){
+        int actualResult=Maximum.getMaximum(3,2,1);
+        Assert.assertEquals(3,actualResult);
+    }
+    @Test
+    public void checkMaximumFloat(){
+        float actualResult=Maximum.getMaximum(3.1f,2.1f,1.1f);
+        Assert.assertEquals(3.1f,actualResult,0.0);
+    }
+    @Test
     public void checkMaximumString(){
         String actualResult=Maximum.getMaximum("ab","cd","ef");
         Assert.assertEquals("cd",actualResult);
     }
-
 }
 
