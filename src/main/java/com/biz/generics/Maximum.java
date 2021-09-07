9913d69e61bdb4551aba2ca09ca7eb84035b7af9
mkaubr007 <mkaubr007@gmail.com>
2021-09-07 23:50:19 +0530

    ExtentMaxWithMethod added

--- src/main/java/com/biz/generics/Maximum.java
+++ src/main/java/com/biz/generics/Maximum.java
@@ -1,28 +1,15 @@
 package com.biz.generics;
 
-public class Maximum<E extends Comparable>{
-    E value1;
-    E value2;
-    E value3;
+public class Maximum{
 
-    public Maximum(E value1, E value2, E value3) {
-        this.value1 = value1;
-        this.value2 = value2;
-        this.value3 = value3;
-    }
-
-    public static  <E extends Comparable> E getMaximum(E a, E b, E c){
-        E max=a;
-        if(b.compareTo(a)>0 && b.compareTo(a)>0){
-            max=b;
-        }else if(c.compareTo(max)>0){
-            max=c;
+    public static  <E extends Comparable> E getMaximum(E...elements) {
+        E max = elements[0];
+        for (E element : elements) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
         }
-        printMax(a,b,c,max);
         return max;
-    }
-    public static <E> void printMax(E a,E b,E c,E max){
-        System.out.println("Max "+a+" "+b+" "+c+" "+max);
     }
 }
 
--- src/test/java/com/biz/generics/MaximumTest.java
+++ src/test/java/com/biz/generics/MaximumTest.java
@@ -6,18 +6,18 @@ import org.junit.Test;
 public class MaximumTest {
     @Test
     public void checkMaximumInteger(){
-        int actualResult=Maximum.getMaximum(3,2,1);
-        Assert.assertEquals(3,actualResult);
+        int actualResult=Maximum.getMaximum(3,2,15,6,8,9);
+        Assert.assertEquals(15,actualResult);
     }
     @Test
     public void checkMaximumFloat(){
-        float actualResult=Maximum.getMaximum(3.1f,2.1f,1.1f);
-        Assert.assertEquals(3.1f,actualResult,0.0);
+        float actualResult=Maximum.getMaximum(3.1f,2.1f,1.1f,9.2f,4.8f,10.8f);
+        Assert.assertEquals(10.8f,actualResult,0.0);
     }
     @Test
     public void checkMaximumString(){
-        String actualResult=Maximum.getMaximum("ab","cd","ef");
-        Assert.assertEquals("cd",actualResult);
+        String actualResult=Maximum.getMaximum("ab","cd","ef","gh","ij");
+        Assert.assertEquals("ij",actualResult);
     }
 }
 
